2d689faf3cf53fb4bb2b7134d05c82b48a5b567c
Aashna <bajwa.aashna@gmail.com>
2019-06-03 09:03:48 -0400

    WIP 1

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -6,30 +6,50 @@ package com.zipcodewilmington.person;
 public class Person {
     private String name;
     private int age;
+    private int height;
+    private int weight;
+    private String eyeColor;
+    private String hairColor;
+    private Boolean isMale;
 
-    public Person() {
+    public Person(String name, int age, int height, int weight, String eyeColor, String hairColor, Boolean isMale) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+        this.eyeColor = eyeColor;
+        this.hairColor = hairColor;
+        this.isMale = isMale;
     }
 
     public Person(int age) {
+        this.age = age;
     }
 
     public Person(String name) {
+        this.name = name;
     }
 
     public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public void setAge(int age) {
+        this.age = age;
     }
 
     public String getName() {
-        return null;
+
+        return name;
     }
 
     public Integer getAge() {
-        return null;
+
+        return age;
     }
 }
